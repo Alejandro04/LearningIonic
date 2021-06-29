70c24c734ab3afad431730511b943b95f2e38861
Alejandro Roa <alejandro.roa@pelikane.com>
2021-06-29 15:00:37 -0400

    add ion avatar and ion chip

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Items {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  public srcAvatar:string;
+  public components: Items[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+    {
+      icon: 'beaker-outline',
+      name: 'Avatar',
+      redirectTo: '/avatar'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    this.srcAvatar = "https://alejodev.com/static/4bacf9c87ae8fdf839e2837d9edf8098/3f23b/profile-pic.avif";
   }
 
 }
